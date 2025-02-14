52b3e6273cdb60fe105f136b1f37574877313fe3
Sebastian <sebastian@familieschlaak.de>
2025-02-14 13:20:55 +0100

    Replace InMemoryVectorStore with Pinecone

--- rag_example.py
+++ rag_example.py
@@ -3,6 +3,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.vectorstores import InMemoryVectorStore
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.document_loaders import DirectoryLoader
+from langchain_pinecone import PineconeVectorStore, Pinecone
 
 loader = DirectoryLoader("data", glob="*.txt")
 documents = loader.load()
@@ -13,9 +14,13 @@ chunks = text_splitter.split_documents(documents)
 
 embeddings = OpenAIEmbeddings()
 
-vectorstore = InMemoryVectorStore.from_documents(chunks, embeddings)
+def create_vectorstore():
+  vectorstore = Pinecone.from_documents(chunks, embeddings, index_name="bike-berlin-demo")
+  return vectorstore
 
 def search_bike_station(query):
+  vectorstore = PineconeVectorStore(index_name="bike-berlin-demo", embedding=embeddings)
   docs = vectorstore.similarity_search(query, k=1)
   return docs[0].page_content
 
+print(search_bike_station("Where is the nearest bike station?"))
