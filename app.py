7e68330ab1eda180b8b1ce8712cbc2b9c6eaf055
Sebastian <sebastian@familieschlaak.de>
2025-02-15 07:53:58 +0100

    Building API endpoint with Flask

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from openai import OpenAI
-from rag_example import search_bike_station
+from rag_example import search_in_vectorstore
 
 client = OpenAI()
 
-user_input = input("Enter your message: ")
-context = search_bike_station(user_input)
-prompt_with_context = f"Context: {context}\n\nUser input: {user_input}"
+def chat(query):
+    context = search_in_vectorstore(query)
+    prompt_with_context = f"Context: {context}\n\nUser input: {query}"
 
-response = client.chat.completions.create(
-    model="o3-mini-2025-01-31",
-    messages=[{"role": "user", "content": prompt_with_context}],
-)
+    response = client.chat.completions.create(
+        model="o3-mini-2025-01-31",
+        messages=[{"role": "user", "content": prompt_with_context}],
+    )
+    return response.choices[0].message.content
 
-print(response.choices[0].message.content)
+print(chat("Where is the nearest bike station? I'm near Berlin Ostbahnhof."))
--- rag_example.py
+++ rag_example.py
@@ -5,22 +5,20 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_community.document_loaders import DirectoryLoader
 from langchain_pinecone import PineconeVectorStore, Pinecone
 
-loader = DirectoryLoader("data", glob="*.txt")
-documents = loader.load()
-
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
-
-chunks = text_splitter.split_documents(documents)
 
 embeddings = OpenAIEmbeddings()
 
 def create_vectorstore():
-  vectorstore = Pinecone.from_documents(chunks, embeddings, index_name="bike-berlin-demo")
-  return vectorstore
+  loader = DirectoryLoader("data", glob="*.txt")
+  documents = loader.load()
 
-def search_bike_station(query):
+  text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+  chunks = text_splitter.split_documents(documents)
+
+  Pinecone.from_documents(chunks, embeddings, index_name="bike-berlin-demo")
+
+
+def search_in_vectorstore(query):
   vectorstore = PineconeVectorStore(index_name="bike-berlin-demo", embedding=embeddings)
   docs = vectorstore.similarity_search(query, k=1)
   return docs[0].page_content
-
-print(search_bike_station("Where is the nearest bike station?"))
